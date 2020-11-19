db09b0eb45fd38ec3107d5df5d98af6aa19aed8c
Anwesh43 <anweshthecool0@gmail.com>
2020-11-19 12:36:01 +0530

    adding BallExpanderRectView class

--- ballrectexpanderview/src/main/java/com/example/ballrectexpanderview/BallExpanderRectView.kt
+++ ballrectexpanderview/src/main/java/com/example/ballrectexpanderview/BallExpanderRectView.kt
@@ -54,3 +54,19 @@ fun Canvas.drawBERNode(i : Int, scale : Float, paint : Paint) {
     paint.color = colors[i]
     drawBallExpanderRect(scale, w, h, paint)
 }
+
+class BallExpanderRectView(ctx : Context) : View(ctx) {
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
