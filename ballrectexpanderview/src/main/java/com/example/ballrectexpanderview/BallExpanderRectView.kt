2ade5caebbe127aee313e886e7a6ef390bedac1c
Anwesh43 <anweshthecool0@gmail.com>
2020-11-19 12:34:34 +0530

    adding Canvas extension methods

--- ballrectexpanderview/src/main/java/com/example/ballrectexpanderview/BallExpanderRectView.kt
+++ ballrectexpanderview/src/main/java/com/example/ballrectexpanderview/BallExpanderRectView.kt
@@ -20,7 +20,6 @@ val colors : Array<Int> = arrayOf(
 }.toTypedArray()
 val parts : Int = 3
 val scGap : Float = 0.02f / parts
-val strokeFactor : Float = 90f
 val rFactor : Float = 7.8f
 val delay : Long = 20
 val backColor : Int = Color.parseColor("#BDBDBD")
@@ -29,3 +28,29 @@ fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
 fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
 fun Float.sinify() : Float = Math.sin(this * Math.PI).toFloat()
+
+fun Canvas.drawBallExpanderRect(scale : Float, w : Float, h : Float, paint : Paint) {
+    val sf : Float = scale.sinify()
+    val sf1 : Float = sf.divideScale(0, parts)
+    val sf2 : Float = sf.divideScale(1, parts)
+    val sf3 : Float = sf.divideScale(2, parts)
+    val r : Float = Math.min(w, h) / rFactor
+    save()
+    translate(w / 2, h / 2)
+    for (j in 0..1) {
+        save()
+        scale(1f - 2 * j, 1f - 2 * j)
+        translate((w / 2 - r) * sf2, 0f)
+        drawCircle(0f, r + (h - 2 * r) * sf3, r * sf1, paint)
+        drawRect(RectF(-r, 0f, r, r + (h - 2 * r) * sf3), paint)
+        restore()
+    }
+    restore()
+}
+
+fun Canvas.drawBERNode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    paint.color = colors[i]
+    drawBallExpanderRect(scale, w, h, paint)
+}
