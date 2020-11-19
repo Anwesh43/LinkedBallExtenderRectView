dadfea57c608d589277dd1a82e80164a3c984a8c
Anwesh43 <anweshthecool0@gmail.com>
2020-11-19 13:00:30 +0530

    adding BallExpanderRect class

--- ballrectexpanderview/src/main/java/com/example/ballrectexpanderview/BallExpanderRectView.kt
+++ ballrectexpanderview/src/main/java/com/example/ballrectexpanderview/BallExpanderRectView.kt
@@ -158,4 +158,27 @@ class BallExpanderRectView(ctx : Context) : View(ctx) {
             return this
         }
     }
+
+    data class BallExpanderRect(var i : Int) {
+
+        private var curr : BERNode = BERNode(0)
+        private var dir : Int = 1
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            curr.draw(canvas, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            curr.update {
+                curr = curr.getNext(dir) {
+                    dir *= -1
+                }
+                cb(it)
+            }
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            curr.startUpdating(cb)
+        }
+    }
 }
